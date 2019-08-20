36a1ac0ced1875a032edd354af5176409ca36e1e
mohammad <allouh.m@gmail.com>
2019-08-20 13:12:51 +0300

    adding list items

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200"
+    tags : ["tag1", "tag2","tag3"]
   };
 
   styles = {
@@ -18,6 +18,9 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+            {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
+        </ul>
       </React.Fragment>
     );
   }
